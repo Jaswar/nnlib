8541b7d20b97ea561e4ac9eaee09c37273fc8557
Jan Warchocki <j.warchocki@student.tudelft.nl>
2022-06-26 15:26:41 +0200

    Remove main.cpp used in testing from .gitignore

---
+++ main.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Jan Warchocki on 28/05/2022.
+//
+
+#include <cstdio>
+#include <iostream>
+#include <activation.h>
+
+int main() {
+
+    return 0;
+}
---
+++ test/main.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Jan Warchocki on 24/06/2022.
+//
+
+#include <gtest/gtest.h>
+
+TEST(suite_name, test_name) {
+    ASSERT_EQ(1, 1);
+}
