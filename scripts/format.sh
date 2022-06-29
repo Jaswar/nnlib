a21c97234aa104f606d40ed1a1267c097e6ad126
Jan Warchocki <j.warchocki@student.tudelft.nl>
2022-06-29 21:49:22 +0200

    Moved format.sh to scripts

--- scripts/format.sh
+++ scripts/format.sh
@@ -1,12 +1,19 @@
+# Navigate to correct directory if script was not moved from "scripts"
+parent_dir=$(pwd | xargs basename)
+if [[ $parent_dir = "scripts" ]]; then
+  cd ..
+fi
+
+# Format all files
 regex="\(\./\(src\|include\)/.*\.\(cpp\|cu\|h\|cuh\)\)\|\(\./test/\(assertions\|utils\)\.\(cpp\|h\)\)"
 num_files=$(find . -regex $regex | wc -l)
-echo ">>> Detected $num_files files to lint"
+echo ">>> Detected $num_files files to format"
 
 files=$(find . -regex $regex)
 echo ">>> Linting $(echo $files | wc -l) files"
 for file in $files; do
     echo ">>> Linting $file"
-    clang-format --dry-run $file
+    clang-format -i -style=file $file
 	
     if [[ $? -ne 0 ]]; then
 	echo ">>> Linter failed for $file"
