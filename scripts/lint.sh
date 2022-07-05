d1470265fd5c92ffc90d246f61beb08ee119d283
Jan Warchocki <j.warchocki@student.tudelft.nl>
2022-07-05 15:01:22 +0200

    Remove CUDA flags from compile commands

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -41,6 +41,11 @@ echo ">>> Building the library for linting"
 cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_TEST_NNLIB=ON ..
 cmake --build .
 
+# Try to remove CUDA compile flags (otherwise clang-tidy throws errors)
+sed -i 's/-forward-unknown-to-host-compiler/ /g' build/compile_commands.json
+sed -i 's/-rdc=true/ /g' build/compile_commands.json
+sed -i 's/--generate-code=arch=compute_[[:digit:]]*,code=\[compute_[[:digit:]]*,sm_[[:digit:]]*\]/ /g' build/compile_commands.json
+
 # Run clang tidy on every wanted file
 echo ">>> Starting clang-tidy, warnings should be ignored"
 cd ..
