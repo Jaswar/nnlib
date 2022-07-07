72f7c1d6375eb9687263b7e70ecf495e81ace568
Jan Warchocki <j.warchocki@student.tudelft.nl>
2022-07-07 16:52:43 +0200

    Vector host operations now use AVX2

--- src/math/vector/vector_operations_on_host.cpp
+++ src/math/vector/vector_operations_on_host.cpp
@@ -3,21 +3,63 @@
 //
 
 #include "vector_operations_on_host.h"
+#ifdef __AVX2__
+#include <immintrin.h>
+#endif
 
 void addVectorsOnHost(const Vector& v1, const Vector& v2, Vector& result) {
+#ifdef __AVX2__
+    for (size_t index = 0; index < v1.n / 8; index++) {
+        __m256 a = _mm256_loadu_ps(v1.data + index * 8);
+        __m256 b = _mm256_loadu_ps(v2.data + index * 8);
+        __m256 res = _mm256_add_ps(a, b);
+        _mm256_store_ps(result.data + index * 8, res);
+    }
+
+    for (size_t index = (v1.n / 8) * 8; index < v1.n; index++) {
+        result[index] = v1[index] + v2[index];
+    }
+#else
     for (int i = 0; i < v1.n; i++) {
         result[i] = v1[i] + v2[i];
     }
+#endif
 }
 
 void subtractVectorsOnHost(const Vector& v1, const Vector& v2, Vector& result) {
+#ifdef __AVX2__
+    for (size_t index = 0; index < v1.n / 8; index++) {
+        __m256 a = _mm256_loadu_ps(v1.data + index * 8);
+        __m256 b = _mm256_loadu_ps(v2.data + index * 8);
+        __m256 res = _mm256_sub_ps(a, b);
+        _mm256_store_ps(result.data + index * 8, res);
+    }
+
+    for (size_t index = (v1.n / 8) * 8; index < v1.n; index++) {
+        result[index] = v1[index] - v2[index];
+    }
+#else
     for (int i = 0; i < v1.n; i++) {
         result[i] = v1[i] - v2[i];
     }
+#endif
 }
 
 void multiplyVectorOnHost(const Vector& v1, DTYPE constant, Vector& result) {
+#ifdef __AVX2__
+    __m256 b = _mm256_set1_ps(constant);
+    for (size_t index = 0; index < v1.n / 8; index++) {
+        __m256 a = _mm256_loadu_ps(v1.data + index * 8);
+        __m256 res = _mm256_mul_ps(a, b);
+        _mm256_store_ps(result.data + index * 8, res);
+    }
+
+    for (size_t index = (v1.n / 8) * 8; index < v1.n; index++) {
+        result[index] = v1[index] * constant;
+    }
+#else
     for (int i = 0; i < v1.n; i++) {
         result[i] = v1[i] * constant;
     }
+#endif
 }
--- test/math/vector_operations_host_tests.cpp
+++ test/math/vector_operations_host_tests.cpp
@@ -8,23 +8,23 @@
 #include "../assertions.h"
 
 TEST(vectorOperationsHost, add) {
-    const Vector& v1 = constructVector({1, 2});
-    const Vector& v2 = constructVector({2, 2});
-    Vector result = Vector(2);
+    const Vector& v1 = constructVector({1, 2, 3, 4, 5, 6, 7, 8, 9, 10});
+    const Vector& v2 = constructVector({1, 2, 3, 4, 5, 6, 7, 8, 9, 10});
+    Vector result = Vector(10);
 
     add(v1, v2, result);
 
-    ASSERT_VECTOR_EQ(result, {3, 4});
+    ASSERT_VECTOR_EQ(result, {2, 4, 6, 8, 10, 12, 14, 16, 18, 20});
 }
 
 TEST(vectorOperationsHost, subtract) {
-    const Vector& v1 = constructVector({1, 2});
-    const Vector& v2 = constructVector({2, 2});
-    Vector result = Vector(2);
+    const Vector& v1 = constructVector({1, 2, 3, 4, 5, 6, 7, 8, 9, 10});
+    const Vector& v2 = constructVector({1, 2, 3, 4, 5, 6, 7, 8, 9, 10});
+    Vector result = Vector(10);
 
     subtract(v1, v2, result);
 
-    ASSERT_VECTOR_EQ(result, {-1, 0});
+    ASSERT_VECTOR_EQ(result, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0});
 }
 
 TEST(vectorOperationsHost, multiplyConstantVector) {
