361036a2b64290dfe90fada42066197634c321f6
Jan Warchocki <j.warchocki@student.tudelft.nl>
2022-07-31 14:52:02 +0200

    Improve check for AVX2, add check for AVX

---
+++ CheckAVXSupport.cmake
@@ -0,0 +1,43 @@
+# Taken from https://gist.github.com/UnaNancyOwen/263c243ae1e05a2f9d0e
+
+include(CheckCXXSourceRuns)
+
+check_cxx_source_runs("
+        #include <immintrin.h>
+        int main()
+        {
+          __m256 a, b, c;
+          const float src[8] = { 1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f, 7.0f, 8.0f };
+          float dst[8];
+          a = _mm256_loadu_ps( src );
+          b = _mm256_loadu_ps( src );
+          c = _mm256_add_ps( a, b );
+          _mm256_storeu_ps( dst, c );
+          for( int i = 0; i < 8; i++ ){
+            if( ( src[i] + src[i] ) != dst[i] ){
+              return -1;
+            }
+          }
+          return 0;
+        }"
+        AVX_SUPPORTED)
+
+check_cxx_source_runs("
+        #include <immintrin.h>
+        int main()
+        {
+          __m256i a, b, c;
+          const int src[8] = { 1, 2, 3, 4, 5, 6, 7, 8 };
+          int dst[8];
+          a =  _mm256_loadu_si256( (__m256i*)src );
+          b =  _mm256_loadu_si256( (__m256i*)src );
+          c = _mm256_add_epi32( a, b );
+          _mm256_storeu_si256( (__m256i*)dst, c );
+          for( int i = 0; i < 8; i++ ){
+            if( ( src[i] + src[i] ) != dst[i] ){
+              return -1;
+            }
+          }
+          return 0;
+        }"
+        AVX2_SUPPORTED)
