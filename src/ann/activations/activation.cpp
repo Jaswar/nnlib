33b22db188a1165ac1ffbe0a2175a0682bd49aa0
Jan Warchocki <j.warchocki@student.tudelft.nl>
2022-09-23 20:29:58 +0200

    Using tensors for activation.cpp

--- src/ann/activations/activation.cpp
+++ src/ann/activations/activation.cpp
@@ -10,8 +10,8 @@
 #include <exceptions/size_mismatch_exception.h>
 #include <utils/location_verifiers.h>
 
-void Activation::forward(const Vector& input, Vector& result) const {
-    if (result.n != input.n) {
+void Activation::forward(const Tensor& input, Tensor& result) const {
+    if (input.shape != result.shape) {
         throw SizeMismatchException();
     }
 
@@ -24,36 +24,8 @@ void Activation::forward(const Vector& input, Vector& result) const {
     }
 }
 
-void Activation::forward(const Matrix& input, Matrix& result) const {
-    if (input.n != result.n || input.m != result.m) {
-        throw SizeMismatchException();
-    }
-
-    if (allLocationsAreHost({input.location, result.location})) {
-        this->hostEvaluator->forward(input, result);
-    } else if (allLocationsAreDevice({input.location, result.location})) {
-        this->deviceEvaluator->forward(input, result);
-    } else {
-        throw DifferentDataLocationException();
-    }
-}
-
-void Activation::computeDerivatives(const Vector& output, Vector& result) const {
-    if (result.n != output.n) {
-        throw SizeMismatchException();
-    }
-
-    if (allLocationsAreHost({output.location, result.location})) {
-        this->hostEvaluator->computeDerivatives(output, result);
-    } else if (allLocationsAreDevice({output.location, result.location})) {
-        this->deviceEvaluator->computeDerivatives(output, result);
-    } else {
-        throw DifferentDataLocationException();
-    }
-}
-
-void Activation::computeDerivatives(const Matrix& output, Matrix& result) const {
-    if (output.n != result.n || output.m != result.m) {
+void Activation::computeDerivatives(const Tensor& output, Tensor& result) const {
+    if (output.shape != result.shape) {
         throw SizeMismatchException();
     }
 
