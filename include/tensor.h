b0ba383d39b47f1804bc56c4fdcc4846a96e0cc7
Jan Warchocki <j.warchocki@student.tudelft.nl>
2022-10-27 23:42:17 +0200

    Documentation for Tensor class

--- include/tensor.h
+++ include/tensor.h
@@ -14,35 +14,120 @@
 #include <iostream>
 #include <vector>
 
+/**
+ * @brief Enumerate to specify where data is located.
+ *
+ * Can be either HOST or DEVICE. In case it is set to HOST, the data is stored in RAM and is processed by the CPU.
+ * In case it is set to DEVICE, the data is in VRAM and processed by the GPU. The latter is only possible if CUDA
+ * is installed and there is a CUDA enabled GPU on the system.
+ */
 enum DataLocation { HOST, DEVICE };
 
+/**
+ * @brief Class to represent multidimensional arrays.
+ */
 class Tensor {
+
+    /**
+     * @brief Store the shape of the tensor.
+     */
 public:
     std::vector<size_t> shape;
+
+    /**
+     * @brief Store the total size of the tensor.
+     *
+     * The size is equal to the number of elements that can be contained in the tensor. It is computed in
+     * the Tensor::computeSize() method.
+     */
     size_t size;
 
+    /**
+     * @brief The location of the tensor.
+     *
+     * Can be either HOST or DEVICE. See #DataLocation for more details.
+     */
     DataLocation location;
+
+    /**
+     * @brief The data stored by the tensor.
+     */
     float* data;
 
+    /**
+     * @brief Session object containing information about current session.
+     */
     Session session;
 
+    /**
+     * @brief Initialize an empty tensor.
+     */
     Tensor();
 
+    /**
+     * @brief The copy constructor.
+     *
+     * @param other The tensor based on which this one should be initialized.
+     */
     Tensor(const Tensor& other);
 
+    /**
+     * @brief Construct a tensor based on the passed shape.
+     *
+     * For example `%Tensor(2, 2, 3)` will initialize a 3D tensor with two 2x3 matrices.
+     *
+     * @param args The shapes of consecutive dimensions.
+     */
     template<typename... Args>
     explicit Tensor(Args... args) : shape({static_cast<size_t>(args)...}), location(HOST), size(0), data() {
         computeSize();
         data = allocate1DArray(size, 0);
     }
 
+    /**
+     * @brief The assignment operator.
+     *
+     * This releases all the current memory and copies all the information of the @p other tensor.
+     *
+     * @param other The other tensor to assign this one to.
+     * @return Assigned tensor. Always returns *this.
+     */
     Tensor& operator=(const Tensor& other);
 
+    /**
+     * @brief Move the tensor to the designated destination.
+     *
+     * This involves copying the data to the new location and releasing memory from the old location.
+     *
+     * @param target The destination to move the tensor to.
+     */
     void move(DataLocation target);
 
+    /**
+     * @brief Static method to easily initialize a 1D tensor with given data.
+     *
+     * @param data The data based on which a tensor should be constructed.
+     * @return The constructed tensor.
+     */
     static Tensor construct1d(const std::vector<float>& data);
+
+    /**
+     * @brief Static method to easily initialize a 2D tensor with given data.
+     *
+     * @param data The data based on which a tensor should be constructed.
+     * @return The constructed tensor.
+     */
     static Tensor construct2d(const std::vector<std::vector<float>>& data);
 
+    /**
+     * @brief Method to access an element of the tensor at a specific method.
+     *
+     * This method should not be used in performance critical operations. In these cases a direct access
+     * to Tensor::data will be more appropriate (as it becomes easier for the compiler to optimize such code).
+     *
+     * @param args The index of the element to access.
+     * @return A reference to the requested element.
+     */
     template<typename... Args>
     float& operator()(Args... args) {
         std::vector<size_t> index = std::vector<size_t>({static_cast<size_t>(args)...});
@@ -53,11 +138,35 @@ public:
         return data[effectiveIndex];
     }
 
+    /**
+     * @brief The destructor.
+     */
     ~Tensor();
 
+    /**
+     * @brief Compute the total size of the tensor based on its shape.
+     */
 private:
     void computeSize();
+
+    /**
+     * @brief Finds the address of an element in the flattened data array given its index in non-flattened tensor.
+     *
+     * Since the method works recursively, it also takes the @p depth parameter, which provides information
+     * about which dimension is currently taken into account when computing the index.
+     *
+     * @param index The multidimensional index of the element to access.
+     * @param depth The dimension that is currently considered in the recursive call.
+     * @return The address of the element in the flattened data array.
+     */
     size_t findEffectiveAddress(const std::vector<size_t>& index, size_t depth) const;
+
+    /**
+     * @brief Verify that an index of an element is within the shape of the tensor.
+     *
+     * @param index The index of the element that is being accessed.
+     * @throws SizeMismatchException If the index is not within the shape of the tensor.
+     */
     void verifyIndex(const std::vector<size_t>& index) const;
 };
 
